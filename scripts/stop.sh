f46b57a4092a72ed6d6dc1ff077ccfed05494ce2
Sunwoong Na <63491090+sunwng@users.noreply.github.com>
2022-12-09 13:39:45 +0900

    CI/CD: edit stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -9,7 +9,8 @@ TIME_NOW=$(date +%c)
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 
-if [ -z $CURRENT_PID ]; then
+if [ -z $CURRENT_PID ]
+then
   echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
 else
   echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
