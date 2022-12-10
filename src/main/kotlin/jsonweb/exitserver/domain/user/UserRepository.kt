a83eceb9b99076333c0f86b8007e3b99e4193ae3
Seong Heung Bae <mopil1102@gmail.com>
2022-12-10 15:02:54 +0900

    feat: 로그아웃, 회원탈퇴 구현

--- src/main/kotlin/jsonweb/exitserver/domain/user/UserRepository.kt
+++ src/main/kotlin/jsonweb/exitserver/domain/user/UserRepository.kt
@@ -2,7 +2,7 @@ package jsonweb.exitserver.domain.user
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository: JpaRepository<User, Long> {
+interface UserRepository : JpaRepository<User, Long>{
     fun existsByNickname(nickname: String): Boolean
     fun findByKakaoId(kakaoId: Long): User?
 }
