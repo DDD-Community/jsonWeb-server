abfe1f9eef2c32afbe71b55396869142435616f4
mopil <mopil1102@gmail.com>
2022-12-16 00:13:34 +0900

    feat: add CORS settings

---
+++ src/main/kotlin/jsonweb/exitserver/common/WebConfig.kt
@@ -0,0 +1,14 @@
+package jsonweb.exitserver.common
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedMethods("*")
+            .allowedOrigins("*")
+    }
+}
