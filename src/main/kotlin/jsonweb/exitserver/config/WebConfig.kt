35a906e84ed4fdfc139c59c0a22711bc9707a3bb
Seong Heung Bae <mopil1102@gmail.com>
2023-01-18 03:46:17 +0900

    feat: add IpLogInterceptor

--- src/main/kotlin/jsonweb/exitserver/config/WebConfig.kt
+++ src/main/kotlin/jsonweb/exitserver/config/WebConfig.kt
@@ -1,15 +1,20 @@
 package jsonweb.exitserver.config
 
+import jsonweb.exitserver.util.IpLogInterceptor
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig : WebMvcConfigurer {
+class WebConfig(private val ipLogInterceptor: IpLogInterceptor) : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedMethods("*")
             .allowedOrigins("*")
-//            .allowCredentials(true)
+    }
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(ipLogInterceptor)
     }
 }
---
+++ src/main/kotlin/jsonweb/exitserver/util/IpLogInterceptor.kt
@@ -0,0 +1,26 @@
+package jsonweb.exitserver.util
+
+import jsonweb.exitserver.common.logger
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+
+
+@Component
+class IpLogInterceptor : HandlerInterceptor {
+    val log = logger()
+    private fun HttpServletRequest.getIp(): String =
+        this.getHeader("X-Forwarded-For")
+            ?: this.getHeader("Proxy-Client-IP")
+            ?: this.getHeader("WL-Proxy-Client-IP")
+            ?: this.getHeader("HTTP_CLIENT_IP")
+            ?: this.getHeader("HTTP_X_FORWARDED_FOR")
+            ?: this.remoteAddr
+
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+        log.info("IP = ${request.getIp()}")
+        return true
+    }
+}
